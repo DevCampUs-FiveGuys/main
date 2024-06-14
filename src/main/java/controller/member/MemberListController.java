a70dba9bff23420a4f30e44b00f1bd8d732328ee
JeenukChung <jeenuk.chung@gmail.com>
2024-06-14 14:51:21 +0900

    init_commit

---
+++ src/main/java/controller/member/MemberListController.java
@@ -0,0 +1,4 @@
+package controller.member;
+
+public class MemberListController {
+}
---
+++ src/test/java/com/example/devcampus/DevCampUsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.devcampus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevCampUsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
