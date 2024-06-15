855f8905d97235087c65e0af28577635e9b19333
PASTELBOX <147224562+PASTELBOX@users.noreply.github.com>
2024-06-15 18:46:25 +0900

    복구완료

--- src/main/java/controller/review/ReviewListController.java
+++ src/main/java/controller/review/ReviewListController.java
@@ -5,8 +5,4 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ReviewListController {
-    @GetMapping("/review")
-    public String list() {
-        return "thymeleaf/review";
-    }
 }
