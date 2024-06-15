d4d2c2e4d564b11e515fa801ccbf0d9482c0c7c6
JeenukChung <jeenuk.chung@gmail.com>
2024-06-15 20:56:30 +0900

    deploy / dockerfile 생성

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,6 @@
-ls -Fhail
+cd ./target
+docker build -t devcampus ../dockerfile
+docker stop devcampus
+docker rm devcampus
+docker rmi devcampus
+docker run -d -p 8060:8080 --name devcampus devcampus
---
+++ dockerfile
@@ -0,0 +1,5 @@
+FROM openjdk:17
+ARG JAR_FILE=DevCampUs-0.0.1-SNAPSHOT.war
+COPY ${JAR_FILE} DevCampUs-0.0.1-SNAPSHOT.war
+EXPOSE 8080
+ENTRYPOINT ["java","-jar","DevCampUs-0.0.1-SNAPSHOT.war"]
