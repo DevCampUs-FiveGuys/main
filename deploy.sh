3c574be992b94d458f20e55be06856527f8d8a79
JeenukChung <jeenuk.chung@gmail.com>
2024-06-15 21:27:57 +0900

    deploy / dockerfile 생성

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,5 @@
-cd ./target
-docker build -t devcampus ../dockerfile
 docker stop devcampus
 docker rm devcampus
 docker rmi devcampus
+docker build -t devcampus .
 docker run -d -p 8060:8080 --name devcampus devcampus
