c2513c72f41aaff932e8a61f5dddd836c50b09c5
Seungpil Huh <gjtmdvlf357@gmail.com>
2024-06-16 17:09:55 +0900

    학생 마이페이지 출석현황 캘린더 및 입실, 퇴실, 휴가신청 버튼 또는 기능 추가 (승필)

--- src/main/java/controller/mypage/MyPageStudentController.java
+++ src/main/java/controller/mypage/MyPageStudentController.java
@@ -5,19 +5,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MyPageStudentController {
-    @GetMapping("/mypage/attendance")
+    @GetMapping("/student_mypage/attendance")
     public String attendance() {
         return "thymeleaf/student/attendance";
     }
-    @GetMapping("/mypage/portfolio_favorites")
+    @GetMapping("/student_mypage/portfolio_favorites")
     public String portfolio_favorites() {
         return "thymeleaf/student/portfolio_favorites";
     }
-    @GetMapping("/mypage/portfolio_posts")
+    @GetMapping("/student_mypage/portfolio_posts")
     public String portfolio_posts() {
         return "thymeleaf/student/portfolio_posts";
     }
-    @GetMapping("/mypage/updateProfile")
+    @GetMapping("/student_mypage/updateProfile")
     public String updateProfile() {
         return "thymeleaf/student/updateProfile";
     }
