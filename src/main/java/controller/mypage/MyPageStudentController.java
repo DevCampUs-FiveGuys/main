68b95103549a9cc437461cf11550ecb63f8a2918
Seungpil Huh <gjtmdvlf357@gmail.com>
2024-06-16 14:26:58 +0900

    경로 수정 및 마이페이지 요쇼들 추가 (승필)

--- src/main/java/controller/mypage/MyPageStudentController.java
+++ src/main/java/controller/mypage/MyPageStudentController.java
@@ -1,7 +1,24 @@
 package controller.mypage;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MyPageStudentController {
+    @GetMapping("/mypage/attendance")
+    public String attendance() {
+        return "thymeleaf/student/attendance";
+    }
+    @GetMapping("/mypage/portfolio_favorites")
+    public String portfolio_favorites() {
+        return "thymeleaf/student/portfolio_favorites";
+    }
+    @GetMapping("/mypage/portfolio_posts")
+    public String portfolio_posts() {
+        return "thymeleaf/student/portfolio_posts";
+    }
+    @GetMapping("/mypage/updateProfile")
+    public String updateProfile() {
+        return "thymeleaf/student/updateProfile";
+    }
 }
