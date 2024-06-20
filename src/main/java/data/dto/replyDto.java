b4c581b1f417161a28b91c5acf8c7785c3f37b39
goni94 <163380530+goni94@users.noreply.github.com>
2024-06-20 10:57:34 +0900

    Sanggon yu (#33)
    * header,css 수정
    
    * header,css 수정
    
    * header,css 수정
    
    * header,css 수정 (상곤)
    
    * header,css 수정 (상곤)
    
    * header,css 수정 (상곤)
    
    * header,css 수정 (상곤)
    
    * header,css 수정 (상곤)
    
    * header,css 수정 (상곤)
    
    * header,css 수정 (상곤)
    
    * header,css 수정 (상곤)
    
    * header,css 수정 (상곤)
    
    * Update style.css
    
    ---------
    
    Co-authored-by: Jinwook Jung <91586765+jeeeenuk@users.noreply.github.com>

--- src/main/java/data/dto/replyDto.java
+++ src/main/java/data/dto/replyDto.java
@@ -1,11 +1,27 @@
 package data.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.apache.ibatis.type.Alias;
+
 import java.sql.Timestamp;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Data
+@Alias("ReplyDto")
 public class replyDto {
     private int reply_id;
     private int member_id;
     private int portfolio_id;
     private String comment;
     private Timestamp created_at;
+    private int readcount;
+    private int regroup;
+    private int restep;
+    private int relevel;
+    private int recount;
 }
