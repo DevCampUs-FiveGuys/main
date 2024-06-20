aafba1c767452b5b6481e58344fb9498723c406b
PASTELBOX <147224562+PASTELBOX@users.noreply.github.com>
2024-06-20 10:32:36 +0900

    리뷰 게시판 수정 (#31)
    * 리뷰게시판 DB 연결
    
    * 리뷰게시판 수강생평점, 점수별비율 추가 및 삭제
    
    * 리뷰게시판 Controller, Mapper, Service 수정
    
    * 좋아요 테이블 관련 Controller, Mapper, Service
    
    * revies.css 정리
    
    * adminmapperInter
    
    ---------
    
    Co-authored-by: alswl11 <parkminji08@naver.com>

--- src/main/java/data/mapper/ReviewMapperInter.java
+++ src/main/java/data/mapper/ReviewMapperInter.java
@@ -1,7 +1,31 @@
 package data.mapper;
 
+import data.dto.ReviewDto;
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
 
 @Mapper
 public interface ReviewMapperInter {
+
+    @Insert("insert into review (content, star, created_at) values (#{content}, #{star}, now())")
+    public void insertReview(ReviewDto reviewDto);
+    @Select("select * from review order by review_id asc")
+    public List<ReviewDto> getAllReview();
+    @Select("select * from review where review_id=#{review_id}")
+    public ReviewDto getReiewData(int review_id);
+    @Delete("delete from review where review_id=#{review_id}")
+    public void deleteReview(int review_id);
+    @Select ("select round(avg(star),1) from review")
+    public  double getAvgStar();
+
+
+//    @Select("select `like` from review where review_id=#{review_id}")
+//    public int getLikeCount(int review_id);
+
+//    @Select("selec")
+
 }
