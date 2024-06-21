dda8f9471b197826bdf48a19c6d8f9ebfc0a135c
PASTELBOX <147224562+PASTELBOX@users.noreply.github.com>
2024-06-21 11:35:47 +0900

    후기 게시판 수정본
    후기 게시판 수정본

--- src/main/java/controller/review/ReviewListController.java
+++ src/main/java/controller/review/ReviewListController.java
@@ -20,16 +20,15 @@ import java.util.stream.Collectors;
 public class ReviewListController {
     @NonNull
     private ReviewService reviewService;
-    @NonNull
-    private CheckListService checkListService;
-
 
     @GetMapping("/review/list")
     public String getReviews(
             Model model) {
-        // Fetch reviews from the database
+
+        // 모든 리뷰를 reviewlist에 추가
         List<ReviewDto> reviewlist = reviewService.getAllReview();
 
+        // 모든 과정명,기수명을 courselist에 추가
         List<CourseDto> courselist = reviewService.getAllCourseList();
 
         // 멤버 전체 명수
@@ -43,7 +42,7 @@ public class ReviewListController {
         String malePercentage = String.format("%.1f", ((double) maleCount / totalGender * 100));
         String femalePercentage = String.format("%.1f", ((double) femaleCount / totalGender * 100));
 
-        // Count the number of reviews in each star range
+        // 평균 평점 계산 영역
         Map<String, Long> starRangeCountMap = reviewlist.stream()
                 .collect(Collectors.groupingBy(
                         review -> {
@@ -64,14 +63,12 @@ public class ReviewListController {
                         }, Collectors.counting()
                 ));
 
-        // Calculate the percentages for each range
         Map<String, String> starRangePercentageMap = starRangeCountMap.entrySet().stream()
                 .collect(Collectors.toMap(
                         Map.Entry::getKey,
                         entry -> String.format("%.1f", ((double) entry.getValue() / reviewlist.size()) * 100)
                 ));
 
-        // Create a LinkedHashMap to maintain the order
         Map<String, String> sortedStarRangePercentageMap = new LinkedHashMap<>();
         List<String> sortedKeys = Arrays.asList("4.5-5", "3.5-4", "2.5-3", "1.5-2", "0.5-1");
         for (String key : sortedKeys) {
@@ -80,7 +77,6 @@ public class ReviewListController {
 
         ReviewDto reviewDto = new ReviewDto();
         model.addAttribute("reviewDto", reviewDto);
-        // Add reviews to the model
         model.addAttribute("reviewlist", reviewlist);
         model.addAttribute("reviewAvg", reviewService.getAvgStar());
         model.addAttribute("starRangePercentageMap", sortedStarRangePercentageMap);
@@ -90,14 +86,11 @@ public class ReviewListController {
 
 //        model.addAttribute("likeCount", checkListService.ShowCountLike(reviewService.getid()));
 //        model.addAttribute("reviewLikeCount", reviewService.getLikeCount(review_id));
-        //checklike dto를 받아와 어차피 dto 안에 rev_id가 있는데 이게 reviewlist의 review_id랑 같은 거잖아?
-        //이거를 == 써가지고 같으면 checklike의 showcountlike를 사용하면 되지 않을까?
-
-
 
         return "thymeleaf/review"; // Return the name of the Thymeleaf template
     }
 
+    // 후기 추가
     @PostMapping("/review/insert")
     public String insertReview(@ModelAttribute ReviewDto reviewDto) {
         System.out.println(reviewDto);
@@ -106,6 +99,7 @@ public class ReviewListController {
         return "redirect:/review/list";
     }
 
+    // 후기 삭제
     @GetMapping("/review/delete")
     public String deleteReview(int review_id) {
         reviewService.deleteReview(review_id);
@@ -113,6 +107,7 @@ public class ReviewListController {
         return "redirect:/review/list";
     }
 
+    //과정명 선택시 get mapping으로 과정명에 해당하는 기수명을 course db에서 불러오기
     @GetMapping("/review/nums")
     @ResponseBody
     public List<String> getCourseNums(@RequestParam("name") String name){
--- src/main/java/data/mapper/CourseMapperInter.java
+++ src/main/java/data/mapper/CourseMapperInter.java
@@ -9,9 +9,11 @@ import java.util.List;
 @Mapper
 public interface CourseMapperInter {
 
+    //중복처리한 과정명을 course db에서 불러오기
     @Select("select distinct name from course")
     public List<CourseDto> getAllCourseList();
 
+    //과정명 선택시 과정명에 해당하는 기수명을 course db에서 불러오기
     @Select("select num from course where name=#{name}")
     public List<String> getNumOfCourse(String name);
 
