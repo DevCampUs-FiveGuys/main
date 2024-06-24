41e895779d4e30708ba79feddf0d0496f7c0348d
Seungpil Huh <78067014+seungpil-huh@users.noreply.github.com>
2024-06-24 11:20:16 +0900

    Seungpil huh (#47)
    * 헤더 및 푸터 수정 (승필)
    
    * 로고 사진 업로드 (승필)
    
    * 경로 수정 및 마이페이지 요쇼들 추가 (승필)
    
    * 학생 마이페이지 출석현황 캘린더 및 입실, 퇴실, 휴가신청 버튼 또는 기능 추가 (승필)
    
    * 학생 마이페이지 출석현황 캘린더 및 입실, 퇴실, 휴가신청 버튼 또는 기능 추가 (승필)
    
    * 학생 마이페이지 출석현황 캘린더 및 입실, 퇴실, 휴가신청 버튼 또는 기능 추가 (승필)
    
    * 학생 마이페이지 출석현황 캘린더 및 입실, 퇴실, 휴가신청 버튼 또는 기능 추가 (승필)
    
    * 서버 포트 8080 문제 해결! (승필)
    
    * 서버 포트 8080 문제 해결! (승필)
    
    * 학생 마이페이지 css 수정 (승필)
    
    * 학생 마이페이지 출석현황 업데이트 (승필)
    
    * 학생 마이페이지 출석현황 업데이트 (승필)
    
    * 학생 마이페이지 출석현황 업데이트 (승필)
    
    * 학생 마이페이지 출석현황 업데이트 (승필)
    
    * 학생 마이페이지 출석현황 업데이트 (승필)
    
    * 학생 마이페이지 출석현황 업데이트 (승필)
    
    * 학생 마이페이지 출석현황 업데이트 (승필)
    
    * 학생 출석현황 페이지에서 입실 및 퇴실 데이터 추가 및 삭제 확인
    
    * 학생 출석현황 페이지에서 출석일수 및 결석일수 업데이트 성공
    
    * 학생 출석현황 페이지에서 지각일수 업데이트 성공
    
    * 학생 출석현황 페이지에서 휴가신청 기능 구현 성공

--- src/main/java/data/service/VacationService.java
+++ src/main/java/data/service/VacationService.java
@@ -1,12 +1,35 @@
 package data.service;
 
+import data.dto.VacationDto;
 import data.mapper.VacationMapperInter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class VacationService {
 
     @Autowired
     private VacationMapperInter vacationMapper;
+
+    // 휴가를 신청하는 메소드
+    public void insertVacation(VacationDto vacationDto) {
+        vacationMapper.insertVacation(vacationDto);
+    }
+
+    // 휴가를 승인하는 메소드
+    public void updateVacation(int member_id) {
+        vacationMapper.updateVacation(member_id);
+    }
+
+    // 휴가 신청을 취소하는 메소드
+    public void deleteVacation(int member_id) {
+        vacationMapper.deleteVacation(member_id);
+    }
+
+    // 모든 휴가 신청 내역을 조회하는 메소드
+    public List<VacationDto> getAllVacation(int member_id) {
+        return vacationMapper.getAllVacation(member_id);
+    }
 }
