c3cd47508fdef75e6e0f5d1d4a76e6414347503d
Jinwook Jung <91586765+jeeeenuk@users.noreply.github.com>
2024-06-26 15:26:56 +0900

    로그인 기능 완벽 구현 (#61)
    * 전체 merge 오류 수정
    
    * 중복체크시 권한 오류 수정
    
    * 회원가입시 생기는 오류 수정
    
    * 로그인 성공, 실패시 실행되는 함수 수정
    
    * 권한오류 설정 변경 !
    
    * 회원가입 최종본 완성 및 권한 수정

--- src/main/java/com/example/devcampus/HomeController.java
+++ src/main/java/com/example/devcampus/HomeController.java
@@ -1,17 +1,34 @@
 package com.example.devcampus;
 
+import data.dto.CourseDto;
+import data.service.ReviewService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 @Controller
+@RequiredArgsConstructor
 public class HomeController {
+    private final ReviewService reviewService;
     @GetMapping("/")
     public String home() {
         return "main";
     }
     @GetMapping("/signup")
-    public String signup() {
+    public String signup(Model model) {
+        List<CourseDto> courselist = reviewService.getAllCourseList();
+        model.addAttribute("courselist", courselist);
         return "thymeleaf/signup";
+    }
+    @GetMapping("/signup/list/names")
+    @ResponseBody
+    public List<String> getCourseNums(@RequestParam("name") String name){
+        return reviewService.getNumOfCourse(name);
     }
     @GetMapping("/login")
     public String login() {
