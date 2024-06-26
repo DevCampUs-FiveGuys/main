5df9ab898595f5ecd7ef4b1ea3429b8e70c18c34
alswl11 <parkminji08@naver.com>
2024-06-26 15:23:32 +0900

    [관리자 마이페이지] 페이지 구현 중

--- src/main/java/data/service/AdminService.java
+++ src/main/java/data/service/AdminService.java
@@ -1,6 +1,7 @@
 package data.service;
 
 
+import data.dto.AttendanceDto;
 import data.dto.MemberDto;
 import data.mapper.AdminMapperInter;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,7 +15,15 @@ public class AdminService {
     @Autowired
     private AdminMapperInter adminMapperInter;
 
+    // 모든 멤버 출력 리스트 (게스트, 학생, 강사)
     public List<MemberDto> getAllMemberList(){
         return adminMapperInter.getAllMemberList();
     }
+    // 학생 출결 현황
+    public List<AttendanceDto> getallattendance(){
+        return adminMapperInter.getallattendance();
+    }
+
+
+
 }
