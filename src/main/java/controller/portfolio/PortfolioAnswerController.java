797443d12aadc4ffb0186855cce0042cce5bb022
You Sang Gon <ybt4794@naver.com>
2024-06-28 09:54:35 +0900

    list,paging (상곤)

--- src/main/java/controller/portfolio/PortfolioAnswerController.java
+++ src/main/java/controller/portfolio/PortfolioAnswerController.java
@@ -25,33 +25,35 @@ public class PortfolioAnswerController {
     @PostMapping("/ainsert")
     public void insertAnswer(@RequestParam int portfolio_id,
                              @RequestParam String comment,
-                             @RequestParam int reply_id,
                              Authentication authentication,
                              Model model)
     {
+
         String email = authentication.getName();
         int member_id = memberService.findByUsername(email).getMember_id();
         String userName = memberService.findByUsername(email).getName();
+        String name = authentication.getName();
 
         model.addAttribute("member_id", member_id);
         model.addAttribute("userName", userName);
-        model.addAttribute("comment", comment);
 
         replyDto dto = replyDto.builder()
                 .comment(comment)
                 .portfolio_id(portfolio_id)
-                .reply_id(reply_id)
+                .member_id(member_id)
                 .member_id(memberService.findByUsername(authentication.getName()).getMember_id())
                 .build();
 
         portfolioAnswerService.insertAnswer(dto);
+
+
     }
 
     @GetMapping("/alist")
-    public List<replyDto> alist(@RequestParam int num)
+    public List<replyDto> alist(@RequestParam int portfolio_id)
 
     {
-        return portfolioAnswerService.getAnswerData(num);
+        return portfolioAnswerService.getAnswerData(portfolio_id);
     }
 
     @GetMapping("/adelete")
