63f230e94645bde138c71882cf448e9527c13a38
Seungpil Huh <gjtmdvlf357@gmail.com>
2024-06-29 19:19:20 +0900

    찜하기 기능 수정

--- src/main/java/data/dto/HeartDto.java
+++ src/main/java/data/dto/HeartDto.java
@@ -6,6 +6,8 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.apache.ibatis.type.Alias;
 
+import java.sql.Timestamp;
+
 @NoArgsConstructor
 @Data
 @Alias("HeartDto")
@@ -15,4 +17,8 @@ public class HeartDto {
     private int heart_id;
     private int member_id;
     private int portfolio_id;
+    private String file_name;     // portfolio 로 부터 가져온 필드
+    private String readcount;     // portfolio 로 부터 가져온 필드
+    private Timestamp created_at; // portfolio 로 부터 가져온 필드
+    private String title;         // portfolio 로 부터 가져온 필드
 }
